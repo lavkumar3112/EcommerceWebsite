7b0d37c3f26fef9f74f8049719e7a0e42b75cfcd
lav <kumarlav59@gmail.com>
2024-03-22 14:34:03 +0530

    add home page carousel and made card section

---
+++ Frontend/E-Commerce/e-commerce/src/customer/components/HomeCaraousel/MainCarouselData.js
@@ -0,0 +1,15 @@
+export const mainCarouselData = [
+    {
+        image:"https://rukminim1.flixcart.com/image/612/612/xif0q/lehenga-choli/y/g/m/l-sleeveless-shset95222-shae-by-sassafras-original-imaggjzkwyyyezzg.jpeg?q=70",
+        path:"/women/clothing/lengha_choli"
+    },
+    {
+        image:"https://rukminim1.flixcart.com/image/612/612/xif0q/dress/a/x/z/l-na-awd-19-yellow-aarvia-original-imagzffm3bkyzup2.jpeg?q=70",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://rukminim1.flixcart.com/image/612/612/xif0q/sari/6/t/z/free-sequined-embroidered-saree-granthva-fab-unstitched-original-imaggsq6b4y2adwk.jpeg?q=70",
+        path:"/women/clothing/women_saree"
+    },
+
+]
